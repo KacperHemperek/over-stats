75e25746e9c0e3d5f26c4fab81b71d7f99153e3e
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-07-07 23:14:17 +0200

    back to cheerio

--- src/routes/api/account/+server.js
+++ src/routes/api/account/+server.js
@@ -1,36 +1,19 @@
 import { error, json } from '@sveltejs/kit';
-// import * as cheerio from 'cheerio';
-import chromium from 'chrome-aws-lambda';
-import playwright from 'playwright-core';
+import * as cheerio from 'cheerio';
 
 /** @type {import('../$types').RequestHandler} */
 export async function GET() {
-	let browser;
 	try {
-		browser = await playwright.chromium.launch({
-			args: chromium.args,
-			executablePath:
-				process.env.NODE_ENV !== 'development'
-					? await chromium.executablePath
-					: '/usr/bin/chromium',
-			headless: process.env.NODE_ENV !== 'development' ? chromium.headless : true
-		});
+		const res = await fetch('https://overwatch.blizzard.com/en-us/career/czitermaster-2477/');
 
-		// const res = await fetch('https://overwatch.blizzard.com/en-us/career/czitermaster-2477/');
+		const html = await res.text();
 
-		// const html = await res.text();
+		const $ = cheerio.load(html);
 
-		// const $ = cheerio.load(html);
+		const name = $('.Profile-player--name').text();
 
-		// const name = $('.Profile-player--name').text();
-		browser.close();
-
-		return json({ hello: 'world' });
+		return json({ name });
 	} catch (err) {
 		throw error(500, 'Internal server error');
-	} finally {
-		if (browser) {
-			await browser.close();
-		}
 	}
 }
