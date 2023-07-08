6114056b2b928e7430b69456bb1c9a76b417efd2
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-07-08 15:05:27 +0200

    add footer to layout

---
+++ src/feaures/footer/footer.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { links } from '$lib/utils/nav-links';
+	import GithubIcon from '../common/github-icon.svelte';
+</script>
+
+<footer class="bg-zinc-950 text-zinc-300 p-4 text-sm text-center">
+	<div class="container lg:max-w-4xl mx-auto px-4">
+		<div class="grid gap-6 md:grid-cols-2">
+			<div class="flex flex-col gap-1">
+				<h5 class="mb-1">Links</h5>
+				{#each links as link}
+					<a href={link.href} class="underline">
+						{link.name}
+					</a>
+				{/each}
+			</div>
+			<div class="flex flex-col gap-1">
+				<h5 class="">Authors</h5>
+				<a
+					target="_blank"
+					href="https://github.com/KacperHemperek"
+					class="flex items-center justify-center underline"
+				>
+					<span class="flex w-4 h-4 mr-1">
+						<GithubIcon />
+					</span>
+					Kacper Hemperek
+				</a>
+				<a
+					target="_blank"
+					href="https://github.com/czitermaster"
+					class="flex items-center justify-center underline"
+				>
+					<span class="flex w-4 h-4 mr-1">
+						<GithubIcon />
+					</span>
+					Kajetan Kacprzyk
+				</a>
+			</div>
+			<p class="text-center text-xs col-span-2">
+				OverStats is an amateur project not affiliated with Blizzard Entertainment.
+			</p>
+		</div>
+	</div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,17 @@
 <script>
 	import './styles.css';
 	import Navbar from '../feaures/navbar/navbar.svelte';
+	import Footer from '../feaures/footer/footer.svelte';
 
 	let showSidebar = false;
 </script>
 
-<Navbar />
+<div class="min-h-screen flex flex-col">
+	<Navbar />
 
-<main class="container lg:max-w-4xl mx-auto px-4">
-	<slot />
-</main>
+	<main class="container lg:max-w-4xl mx-auto px-4 flex-grow mb-12">
+		<slot />
+	</main>
+
+	<Footer />
+</div>
