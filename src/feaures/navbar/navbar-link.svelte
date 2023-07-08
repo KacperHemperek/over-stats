a51812bbf359a137d07d4f91c5d2ec6ec8fb5373
czitermaster <kajtulo66@wp.pl>
2023-07-08 13:24:18 +0200

    navbar-link component

---
+++ src/feaures/navbar/navbar-link.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { page } from '$app/stores';
+	/**
+	 * @type {String}
+	 */
+	export let href;
+	/**
+	 * @type {string}
+	 */
+	export let name;
+
+	$: active = $page.url.pathname === href;
+
+	console.log($page.url.pathname);
+</script>
+
+<a
+	{href}
+	class="{active
+		? 'text-orange-500'
+		: 'text-zinc-50'} text-2xl py-2 text-center font-semibold uppercase hover:text-orange-500 transition duration-200 lg:text-base lg:py-0"
+	>{name}</a
+>
