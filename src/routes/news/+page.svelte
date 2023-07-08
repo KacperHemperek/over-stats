ab4b8bafd3cef4bc19d4aaabf838012f221e72fc
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-07-08 10:26:29 +0200

    create news page and header section

---
+++ src/routes/news/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import SectionHeader from '../../feaures/common/section-header.svelte';
+</script>
+
+<SectionHeader>
+	<span slot="title">NEWS</span>
+	<span slot="description"
+		>Latest news from <span class="text-orange-500">overwatch</span>, new maps, skins, battle pass
+		and more</span
+	>
+</SectionHeader>
