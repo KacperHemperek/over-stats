5962e3861802c94a497803e5a516ab0b69f5cf52
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-07-08 13:55:44 +0200

    remove unused imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,5 @@
 <script>
 	import './styles.css';
-	import MobileNavButton from '../feaures/navbar/mobile-nav-button.svelte';
-	import { fly } from 'svelte/transition';
-	import { cubicOut } from 'svelte/easing';
-	import { navigating } from '$app/stores';
-	import NavbarLink from '../feaures/navbar/navbar-link.svelte';
 	import Navbar from '../feaures/navbar/navbar.svelte';
 
 	let showSidebar = false;
