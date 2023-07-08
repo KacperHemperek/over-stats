7e9cc93663b617e2aa4f1e72d88f4c27de1f2b7e
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-07-08 17:02:24 +0200

    show tooltip when search input is invalid

---
+++ src/feaures/common/tooltip.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { fly } from 'svelte/transition';
+
+	/** @type{string} */
+	export let title;
+
+	/** @type{string} */
+	export let message;
+</script>
+
+<div
+	class="p-4 max-w-xs fixed bottom-8 right-1/2 translate-x-1/2 bg-red-500"
+	in:fly={{ y: 200, duration: 200, opacity: 1 }}
+	out:fly={{ y: 200, duration: 200, opacity: 1 }}
+>
+	<h4>{title}</h4>
+	<p>{message}</p>
+</div>
--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -2,14 +2,25 @@
 	import { goto } from '$app/navigation';
 	import Searchbar from '../../feaures/common/searchbar.svelte';
 	import SectionHeader from '../../feaures/common/section-header.svelte';
+	import Tooltip from '../../feaures/common/tooltip.svelte';
 
 	let searchQuery = '';
+	let timeout = null;
+	let showTooltip = false;
 
 	const handleSearch = (event) => {
 		if (event.detail.isValid) {
 			goto(`/account/${event.detail.username}-${event.detail.tag}`);
 		} else {
-			console.log('invalid search');
+			showTooltip = true;
+			if (timeout) {
+				console.log('clearing timeout');
+				clearTimeout(timeout);
+			}
+			timeout = setTimeout(() => {
+				console.log('hidint tooltip');
+				showTooltip = false;
+			}, 2000);
 		}
 	};
 </script>
@@ -24,3 +35,10 @@
 </SectionHeader>
 
 <Searchbar bind:value={searchQuery} on:search={handleSearch} />
+
+{#if showTooltip}
+	<Tooltip
+		message="Your username is invalid, keep in mind you have to pass username like so: username#1234"
+		title="Search error"
+	/>
+{/if}
