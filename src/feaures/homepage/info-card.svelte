7b92f108ad658ceaf928c4ff3bdcc9cdd4ca7074
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-07-08 12:23:38 +0200

    make info card link to page

--- src/feaures/homepage/info-card.svelte
+++ src/feaures/homepage/info-card.svelte
@@ -3,9 +3,11 @@
 	export let title;
 	/** @type {string} -- description to display in card body */
 	export let description;
+	/** @type {string} -- link for page to redirect in click */
+	export let href;
 </script>
 
-<div class="flex justify-cente border-2 border-zinc-600 p-4 flex-col">
+<a {href} class="flex justify-cente border-2 border-zinc-600 p-4 flex-col">
 	<div class=" flex items-center gap-1 mb-2">
 		<h2 class="text-lg font-bold text-orange-500">{title}</h2>
 		<div class="w-4 h-4 flex text-zinc-500">
@@ -15,4 +17,4 @@
 	<p class="text-zinc-500">
 		{description}
 	</p>
-</div>
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,34 +18,37 @@
             class="lucide lucide-search"
             ><circle cx="11" cy="11" r="8" />
             <path d="m21 21-4.3-4.3" />
-          </svg>`
+          </svg>`,
+			href: '/search'
 		},
 		{
 			title: 'Track',
 			description: 'Track your stats and see how you are doing in ranked and unranked matches',
 			icon: `
       <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-		  fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="lucide lucide-bar-chart-4"
-    >
-      <path d="M3 3v18h18" />
-      <path d="M13 17V9" />
-      <path d="M18 17V5" />
-      <path d="M8 17v-3" />
-    </svg>`
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-bar-chart-4"
+      >
+        <path d="M3 3v18h18" />
+        <path d="M13 17V9" />
+        <path d="M18 17V5" />
+        <path d="M8 17v-3" />
+      </svg>`,
+			href: '/track'
 		},
 		{
 			title: 'News',
 			description:
 				'Stay up-to-date with the latest features, updates, and exciting additions to Overwatch 2.',
 			icon: `
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bell"><path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"/><path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"/></svg>`
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bell"><path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9"/><path d="M10.3 21a1.94 1.94 0 0 0 3.4 0"/></svg>`,
+			href: '/news'
 		}
 	];
 </script>
@@ -68,7 +71,7 @@
 	<h1 class="text-2xl font-bold text-center uppercase mb-8">Features</h1>
 	<div class="grid mb-6 gap-6 md:grid-cols-3">
 		{#each homepageCards as card}
-			<InfoCard title={card.title} description={card.description}>
+			<InfoCard title={card.title} description={card.description} href={card.href}>
 				{@html card.icon}
 			</InfoCard>
 		{/each}
